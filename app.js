33d025a944e6300d43b17fbc6ac29e22a4db3f7d
mahnoorfatima <mahnoorfatimabaloch94@hotmail.com>
2020-10-20 20:22:17 +0500

    dummy app.js

--- app.js
+++ app.js
@@ -1,37 +1,58 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+// const passport = require("passport");
+// const serverless = require('serverless-http');
+// const routes = require("./routes/api/routes");
+// const app = express();
+// const cors = require('cors');
+// app.use(cors())
+
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false
+//   })
+// );
+// app.use(bodyParser.json());
+// // DB Config
+// const db = require("./config/keys").mongoURI;
+// // Connect to MongoDB
+// mongoose
+//   .connect(
+//     db,
+//     { useNewUrlParser: true }
+//   )
+//   .then(() => console.log("MongoDB successfully connected"))
+//   .catch(err => console.log(err));
+// // Passport middleware
+// app.use(passport.initialize());
+// // Passport config
+// require("./config/passport")(passport);
+// // Routes
+// app.use("/api", routes);
+// // const port = process.env.PORT || 5000;
+// // app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
+// module.exports.handler = serverless(app);
+
+
+'use strict';
+const express = require('express');
 const serverless = require('serverless-http');
-const routes = require("./routes/api/routes");
 const app = express();
-const cors = require('cors');
-app.use(cors())
+const bodyParser = require('body-parser');
 
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
 app.use(bodyParser.json());
-// DB Config
-const db = require("./config/keys").mongoURI;
-// Connect to MongoDB
-mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
-// Passport middleware
-app.use(passport.initialize());
-// Passport config
-require("./config/passport")(passport);
-// Routes
-app.use("/api", routes);
-// const port = process.env.PORT || 5000;
-// app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 
+module.exports = app;
 module.exports.handler = serverless(app);
-
